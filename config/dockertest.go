c421730c0e9c776b9f3bbbe9beb421d2f0790a7d
gaurav-nagpal-new <gnagpal119@gmail.com>
2024-06-21 08:02:02 +0530

    correct dockertest version

--- config/dockertest.go
+++ config/dockertest.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/ory/dockertest"
+	"github.com/ory/dockertest/v3"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.uber.org/zap"
